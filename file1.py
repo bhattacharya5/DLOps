55eb3828b2dd277a69e13de2d27922238f7149dd
bhattacharya5 <bhattacharya.5@iitj.ac.in>
2024-03-30 12:06:50 +0530

    CNN model configurations modofied to normaization layer and global average pooling

--- file1.py
+++ file1.py
@@ -6,20 +6,25 @@ from tensorflow.keras import layers, models
 # Define the random values
 randomValues = np.array([-3.5, -1.2, 0, 3.2, -3.8, 1.7, -0.6, 3.3, -2.6, 5.6])
 
-# Define the CNN model
+
+# Model Configuration consists of three convolutional layers with increasing filter sizes.
+# Batch normalization layers are added after each convolutional layer to normalize the activations.
+# Global average pooling is used instead of flattening before the final dense layer.
 model = models.Sequential([
     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-    layers.MaxPooling2D((2, 2)),
+    layers.BatchNormalization(),
     layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.Flatten(),
-    layers.Dense(64, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.BatchNormalization(),
+    layers.GlobalAveragePooling2D(),
     layers.Dense(10, activation='softmax')
 ])
+
 
 # Print model summary
 model.summary()
 
 plt.tight_layout()
 plt.show()
+
